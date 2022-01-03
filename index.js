7137f867d8426ab28b68d652e701917ccdec9a5a
Adankanych <67391574+Adankanych@users.noreply.github.com>
2022-01-03 00:13:53 -0500

    updated daily forecast to show live information

--- index.js
+++ index.js
@@ -23,23 +23,23 @@ function formatDate(timestamp) {
 }
 
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let days = ["Sat", "Sun", "Mon"];
   let forecastHTML = `<div class="row">`;
 
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `<div class="col-2">
-        <div class="weather-forcaste-date">${day}</div>
+        <div class="weather-forcaste-date">${forecastDay.dt}</div>
               <img
-                src="http://openweathermap.org/img/wn/50d@2x.png"
+                src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
                 width="36"
               />
               <div class="weather-forecast-temperature">
-                <span class="weather-forecast-temperature-max">18°</span>
-                <span class="weather-forecast-temperature-min">12°</span>
+                <span class="weather-forecast-temperature-max">${forecastDay.temp.max}</span>
+                <span class="weather-forecast-temperature-min">${forecastDay.temp.min}</span>
               </div>
             </div>
         `;
